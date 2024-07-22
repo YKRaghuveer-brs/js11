e58a1299182d7ca436c8bc226eee83ddd0646601
unknown <yelurikesavaraghuveer@gmail.com>
2024-07-22 09:02:43 +0530

    canvas commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ const canvas = document.getElementById("draw");
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
-const ctx = canvas.getContext("2d"); // 2 prams - context type and context attributes
+const ctx = canvas.getContext("2d");
 
 // Default theme
 let chathams_blue = "#1A4B84";
@@ -29,34 +29,35 @@ let lastY = 0;
 let hue = 0;
 
 function draw(e) {
-  if (!isDrawing) return; // Check for mouse click
-  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    if (!isDrawing) return; // Check for mouse click
+    ctx.strokeStyle = document.getElementById('colorPicker').value || `hsl(${hue}, 100%, 50%)`;
 
-  ctx.beginPath(); //Begin a new path
+    ctx.beginPath(); //Begin a new path
 
-  // Start drawing the line
-  ctx.moveTo(lastX, lastY);
-  console.log(`LAST X - ${lastX}`);
-  console.log(`LAST Y - ${lastY}`);
+    // Start drawing the line
+    ctx.moveTo(lastX, lastY);
 
-  // Go to current mouse location
-  ctx.lineTo(e.offsetX, e.offsetY);
-  console.log(`CURRENT X - ${e.offsetX}`);
-  console.log(`CURRENT Y - ${e.offsetY}`);
+    // Go to current mouse location
+    ctx.lineTo(e.offsetX, e.offsetY);
 
-  ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY];
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 
-  hue++;
-  if (hue >= 360) {
-    hue = 0;
-  }
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
 }
+
+// Clear canvas
+document.getElementById('clearCanvas').addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
 // Event Listeners
 canvas.addEventListener("mousedown", (e) => {
-  isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY]; //Mouse cursor's coordinates
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY]; //Mouse cursor's coordinates
 });
 
 canvas.addEventListener("mousemove", draw);
@@ -65,16 +66,7 @@ canvas.addEventListener("mouseout", () => (isDrawing = false));
 
 // Set theme
 function setTheme(theme) {
-  document.documentElement.style.setProperty("--primary-color", theme);
-  localStorage.setItem("movie-theme", theme);
+    document.documentElement.style.setProperty("--primary-color", theme);
+    localStorage.setItem("movie-theme", theme);
 }
 setTheme(localStorage.getItem("movie-theme") || chathams_blue);
-
-console.log("hello world");
-
-let num = 10;
-num == 10;
-
-function addNum(a = 10, b = 20) {
-  return a + b;
-}
